dea04744e6058d41556d4be50659db73a90bb4fd
Gerald Joseph <geraldjosephk@gmail.com>
2022-03-07 07:22:12 +0300

    corrected error in carts model
    Renamed card_id to cart_id in class Cart

--- GreatKart/carts/models.py
+++ GreatKart/carts/models.py
@@ -5,7 +5,7 @@ from store.models import Product
 
 
 class Cart(models.Model):
-    card_id = models.CharField(max_length=250, blank=True)
+    cart_id = models.CharField(max_length=250, blank=True)
     date_added = models.DateField(auto_now_add=True)
 
     def __str__(self):
