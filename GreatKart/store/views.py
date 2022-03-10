99cc654394a9c4c793ada81452a78e9ddf9d9aba
Gerald Joseph <geraldjosephk@gmail.com>
2022-03-10 11:52:06 +0300

    Fixing Cart bugs @ context processor for cart itemcounter
    Check for empty cart, fixed add to cart links,check if product was added to cart, counter context processor for cart icon in navbar

--- GreatKart/store/views.py
+++ GreatKart/store/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render, get_object_or_404
 from category.models import Category
 from .models import Product
+from carts.models import CartItem
+from carts.views import _cart_id
+# from django.http import HttpResponse
 
 # Create your views here.
 
@@ -31,11 +34,16 @@ def product_detail(request, category_slug, product_slug):
     try:
         single_product = Product.objects.get(
             category__slug=category_slug, slug=product_slug)
+        in_cart = CartItem.objects.filter(
+            cart__cart_id=_cart_id(request), product=single_product).exists()  # Check if item is already available in the cart
+        # return HttpResponse(in_cart)
+        # exit()
     except Exception as e:
         raise e
 
     context = {
         'single_product': single_product,
+        'in_cart': in_cart,
     }
 
     return render(request, 'store/product_detail.html', context)
