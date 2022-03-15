8e907b2c3992a6c1ce331685823611e202ea21d2
Gerald Joseph <geraldjosephk@gmail.com>
2022-03-15 11:05:24 +0300

    View Product details, Enabled paginator feature Fixing Products warning and Empty Cart and Enabled Search function

--- GreatKart/carts/views.py
+++ GreatKart/carts/views.py
@@ -3,8 +3,9 @@ from multiprocessing import context
 from django.shortcuts import render, redirect, get_object_or_404
 from store.models import Product
 from .models import Cart, CartItem
+from django.core.exceptions import ObjectDoesNotExist
 
-from django.http import HttpResponse
+# from django.http import HttpResponse
 
 # Create your views here.
 
@@ -73,6 +74,8 @@ def remove_cart_item(request, product_id):
 def cart(request, total=0, quantity=0, cart_items=None):
     """Function to display what is avaible in the cart"""
     try:
+        tax = 0
+        grand_total = 0
         cart = Cart.objects.get(cart_id=_cart_id(
             request))  # get cart session id
         # filter products in present the cart
@@ -83,7 +86,7 @@ def cart(request, total=0, quantity=0, cart_items=None):
             quantity += cart_item.quantity  # Get current number of items
         tax = (2*total)/100
         grand_total = total + tax
-    except ObjectNotExist:
+    except ObjectDoesNotExist:
         pass  # If no items available in the cart,ignore
 
     context = {
--- GreatKart/store/views.py
+++ GreatKart/store/views.py
@@ -1,28 +1,39 @@
+from ast import keyword
 from django.shortcuts import render, get_object_or_404
 from category.models import Category
 from .models import Product
 from carts.models import CartItem
 from carts.views import _cart_id
+from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
+from django.db.models import Q
 # from django.http import HttpResponse
 
 # Create your views here.
 
 
 def store(request, category_slug=None):
+    """Function to load available products in store"""
     categories = None
     products = None
 
     if category_slug != None:
         categories = get_object_or_404(Category, slug=category_slug)
+        # Refer to the Django docs and search Pagination
         products = Product.objects.filter(
             category=categories, is_available=True)
+        paginator = Paginator(products, 1)
+        page = request.GET.get('page')
+        paged_products = paginator.get_page(page)
         product_count = products.count()
     else:
-        products = Product.objects.all().filter(is_available=True)
+        products = Product.objects.all().filter(is_available=True).order_by('id')
+        paginator = Paginator(products, 3)
+        page = request.GET.get('page')
+        paged_products = paginator.get_page(page)
         product_count = products.count()
 
     context = {
-        'products': products,
+        'products': paged_products,
         'product_count': product_count,
     }
     return render(request, 'store/store.html', context)
@@ -47,3 +58,21 @@ def product_detail(request, category_slug, product_slug):
     }
 
     return render(request, 'store/product_detail.html', context)
+
+
+def search(request):
+    """Function to checks the keyword value from the search url"""
+    if 'keyword' in request.GET:
+        keyword = request.GET['keyword']
+        if keyword:
+            # Filter product descriptions or product names containing the keyword
+            products = Product.objects.order_by('-created_date').filter(
+                Q(description__icontains=keyword) | Q(product_name__icontains=keyword))
+            product_count = products.count()
+
+    context = {
+        'products': products,
+        'product_count': product_count,
+        'keyword': keyword,
+    }
+    return render(request, 'store/store.html', context)
