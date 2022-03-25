13aa0da55650ccbac9c512913f7b9a44e3073024
Gerald Joseph <geraldjosephk@gmail.com>
2022-03-25 05:51:07 +0300

    fixed bug in store/models.py

--- GreatKart/store/models.py
+++ GreatKart/store/models.py
@@ -1,7 +1,5 @@
 from ast import arg
 from audioop import reverse
-from itertools import product
-from unicodedata import category
 from django.db import models
 from category.models import Category
 from django.urls import reverse
@@ -11,11 +9,12 @@ from django.urls import reverse
 
 class Product(models.Model):
     """Product model"""
+
     product_name = models.CharField(max_length=200, unique=True)
     slug = models.SlugField(max_length=200, unique=True)
     description = models.TextField(max_length=500, blank=True)
     price = models.IntegerField()
-    images = models.ImageField(upload_to='photos/Products')
+    images = models.ImageField(upload_to="photos/Products")
     stock = models.IntegerField()
     is_available = models.BooleanField(default=True)
     # Deletes entire product category
@@ -25,7 +24,7 @@ class Product(models.Model):
 
     def get_url(self):
         """Function to link products in store to single product view page"""
-        return reverse('product_detail', args=[self.category.slug, self.slug])
+        return reverse("product_detail", args=[self.category.slug, self.slug])
 
     def __str__(self):
         return self.product_name
@@ -35,23 +34,29 @@ class VariationManager(models.Manager):
     """Class model to list different variations"""
 
     def colors(self):
-        return super(VariationManager, self).filter(variation_category='color', is_active=True)
+        return super(VariationManager, self).filter(
+            variation_category="color", is_active=True
+        )
 
     def sizes(self):
-        return super(VariationManager, self).filter(variation_category='size', is_active=True)
+        return super(VariationManager, self).filter(
+            variation_category="size", is_active=True
+        )
 
 
 variation_category_choice = (
-    ('color', 'color'),
-    ('size', 'size'),
+    ("color", "color"),
+    ("size", "size"),
 )
 
 
 class Variation(models.Model):
     """Class Model for storing product variations"""
+
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     variation_category = models.CharField(
-        max_length=100, choices=variation_category_choice)
+        max_length=100, choices=variation_category_choice
+    )
     variation_value = models.CharField(max_length=100)
     is_active = models.BooleanField(default=True)
     created_date = models.DateTimeField(auto_now=True)
@@ -59,7 +64,7 @@ class Variation(models.Model):
     objects = VariationManager()
 
     # def __unicode__(self):
-    #     return self.product
+    #     return self.variation_value
 
     def __str__(self):  # for displaying variations properly in Django Admin
-        return self.product
+        return self.variation_value
