e96f03a7b5505b1853d854d65e815d72d99ea34e
Gerald Joseph <geraldjosephk@gmail.com>
2022-04-04 07:04:57 +0300

    checkout page, views and url

--- GreatKart/carts/views.py
+++ GreatKart/carts/views.py
@@ -165,3 +165,29 @@ def cart(request, total=0, quantity=0, cart_items=None):
         "grand_total": grand_total,
     }
     return render(request, "store/cart.html", context)
+
+
+def checkout(request, total=0, quantity=0, cart_items=None):
+    try:
+        tax = 0
+        grand_total = 0
+        cart = Cart.objects.get(cart_id=_cart_id(request))  # get cart session id
+        # filter products in present the cart
+        cart_items = CartItem.objects.filter(cart=cart, is_active=True)
+        for cart_item in cart_items:
+            # get total price of all items in cart
+            total += cart_item.product.price * cart_item.quantity
+            quantity += cart_item.quantity  # Get current number of items
+        tax = (2 * total) / 100
+        grand_total = total + tax
+    except ObjectDoesNotExist:
+        pass  # If no items available in the cart,ignore
+
+    context = {
+        "total": total,
+        "quuantity": quantity,
+        "cart_items": cart_items,
+        "tax": tax,
+        "grand_total": grand_total,
+    }
+    return render(request, "store/checkout.html", context)
