a65f929ec0cbbfb28d077afb8eabe8069a07f84d
Gerald Joseph <geraldjosephk@gmail.com>
2022-05-17 20:52:00 +0300

    Edit profile setup

--- GreatKart/accounts/urls.py
+++ GreatKart/accounts/urls.py
@@ -19,4 +19,6 @@ urlpatterns = [
     path("resetPassword", views.forgotPassword, name="resetPassword"),
     # Orders
     path("my_orders/", views.my_orders, name="my_orders"),
+    # Edit profile
+    path("edit_profile/", views.edit_profile, name="edit_profile"),
 ]
