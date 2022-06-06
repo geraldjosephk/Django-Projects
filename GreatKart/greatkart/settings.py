427e6ae9ff7aa4df7bdcc111d9c8ce82ce73f089
Gerald Joseph <geraldjosephk@gmail.com>
2022-06-06 14:04:48 +0300

    Log out after inactivity

--- GreatKart/greatkart/settings.py
+++ GreatKart/greatkart/settings.py
@@ -46,7 +46,7 @@ INSTALLED_APPS = [
     "store",
     "carts",
     "orders",
-    'admin_honeypot',
+    "admin_honeypot",
 ]
 
 MIDDLEWARE = [
@@ -57,7 +57,13 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "django_session_timeout.middleware.SessionTimeoutMiddleware",
 ]
+
+SESSION_EXPIRE_SECONDS = 3600  # 1 hour
+SESSION_EXPIRE_AFTER_LAST_ACTIVITY = True
+SESSION_EXPIRE_AFTER_LAST_ACTIVITY_GRACE_PERIOD = 60  # group by minute
+SESSION_TIMEOUT_REDIRECT = "accounts/login"
 
 ROOT_URLCONF = "greatkart.urls"
 
